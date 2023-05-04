a4409a5dc40fcf93e8b0285604996e8a32989d87
tylerwarwick <tylerwarwickwork@gmail.com>
2023-05-03 23:58:18 -0400

    Form construction

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,11 +6,37 @@ interface ButtonProps{
   displayTag : string;
 }
 
+interface FormProps{
+  actionLocation : string;
+}
+
+//Form
+const Form = ({actionLocation} : FormProps) => {
+  return (
+  <form>
+    <div className='p-3 m-0'>
+      <label className='p-3 font-bold'>Email</label>
+      <input type="email" placeholder='youremail@hotdog.com' name="email" className='border-2 border-blue-500 font-bold rounded'/>
+    </div>
+    <br></br>
+    <div className='mx-3 m-0'>
+      <label className='p-3 font-bold'>Password</label>
+      <input type="password" placeholder='example password' name="password" className='border-2 border-blue-500 rounded'/>
+    </div>
+  </form>
+  )
+}
+
+
+
 // Login form
 function LoginForm() {
   return (
-      <div className="h-screen flex items-center justify-center">
-          <Button displayTag={"Click me"}/>
+      <div className='box-border w-1/2 h-1/2 p-4 border-4'>
+        <div>
+          <Form actionLocation='pig'/>
+          <Button displayTag='Click me'/> 
+        </div>
       </div>
   )
 }
@@ -29,8 +55,8 @@ function App() {
         
       </header>
 
-    <div>
-    <LoginForm/>
+    <div className='h-screen flex justify-center items-center'>
+      <LoginForm/>
     </div>
 
 
