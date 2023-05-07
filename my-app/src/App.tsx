a283c578cd5ad7d79a1f32b613ce28020c2fe50d
tylerwarwick <tylerwarwickwork@gmail.com>
2023-05-07 17:11:07 -0400

    Restructure of files

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import {LoginPage} from './views';
+import Navbar from './components/navbar'
+import Card from './components/card';
 
 
 function App() {
   return (
-    <LoginPage/>
+    <Navbar />
+  
   );
 }
 
