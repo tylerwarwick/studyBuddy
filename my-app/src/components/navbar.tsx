6bf366c1f6521916c28e41695c9ca5dd6f4a3334
tylerwarwick <tylerwarwickwork@gmail.com>
2023-05-10 22:04:15 -0400

    Active navlinks finished

--- my-app/src/components/navbar.tsx
+++ my-app/src/components/navbar.tsx
@@ -39,13 +39,11 @@ export default function Navbar() {
         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul id='navbar' className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             
             <NavLinkStyled DisplayTag='Login' Location='/login'/>
             <NavLinkStyled DisplayTag='Register' Location='/register'/>
-            <NavLinkStyled DisplayTag='About' Location='#'/>
-            <NavLinkStyled DisplayTag='Contact' Location='#'/>
-
+    
         </ul>
         </div>
     </div>
