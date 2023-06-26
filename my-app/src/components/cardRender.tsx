110e8364e82d9a13326ec5d3dd4535f3fff99c8d
tylerwarwick <tylerwarwickwork@gmail.com>
2023-06-25 22:34:21 -0400

    Need to fix bug/learn how to alter state array of objects with json payload from db

---
+++ my-app/src/components/cardRender.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import '../App.css';
+import { text } from 'stream/consumers';
+
+//Need to accept function as prop so that we can update parent state from child
+interface CardProps {
+    displayText : string;
+    func : () => void;
+
+}
+
+
+
+export default function CardRender({displayText : text, func : flip} : CardProps) {
+    return (
+        <a onClick={flip} className="block w-96 h-64 flex items-center justify-center p-16 border rounded-lg shadow bg-gray-800 border-gray-700 hover:bg-gray-700">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white text-center">{text}</h5>
+        </a>
+    );
+
+
+}
--- my-app/src/pages/practice.tsx
+++ my-app/src/pages/practice.tsx
@@ -1,49 +1,78 @@
 import '../App.css';
-import {useState} from 'react';
-import Card from '../components/card';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+import CardRender from '../components/cardRender';
 import Button from '../components/button';
-import { CardProps } from '../components/card';
 
+interface Card {
+    id : number;
+    question : string;
+    answer : string;
+    isKnown : boolean;
+}
 
-
-const cards = [
-    {
-        "question" : "What is the powerhouse of the cell?",
-        "answer" : "Mitochondria",
-        "isKnown" : false
-    },
-    {
-        "question": "What makes plant cells unique?",
-        "answer": "Plant cells have a vacuole to maintain turgor pressure",
-        "isKnown": false
-    },
-    {
-        "question" : "Do my balls itch?",
-        "answer" : "Yes",
-        "isKnown": false
-    }
-    ];
-
-
-    
+const c1 : Card = {
+    id : 10,
+    question : "How r u?",
+    answer : "Shitty",
+    isKnown : false
+}
 
 const Practice = () => {
-    const [index, updateIndex] = useState(0);
-    let deckLength = cards.length;
+    const [cards, setCards] = useState<Card[]>([])
 
+   
+    axios.get("http://localhost:3001/notes")
+    .then(Response => {
+        
+        let temp : Card[] = Response.data as Card[];
+        console.log(temp)
+        setCards(current => [...current, c1]);
+        console.log(cards);
+    })
+    
+
+
+    //Get cards that aren't memorized yet
+    const checkIsKnown = (card : Card) => {if (!card.isKnown) return card};    
+    let activeCards = cards.filter(checkIsKnown);
+
+    //Store length of question deck to infinitely cycle through deck until empty
+    const [index, updateIndex] = useState(0);
+    let deckLength = activeCards.length;
+
+    //Define state for which way card is facing
+    //Pass function to update state to child card component
+    const [frontFacing, updateDirection] = useState(true);
+    const flipCard = () => updateDirection(frontFacing => !frontFacing);
+
+    //Loop through deck forever
     const loopIndex = () => {return index == deckLength - 1 ? 0 : index + 1};
-    const handleClick = (e : React.MouseEvent<HTMLButtonElement>) => updateIndex(loopIndex);
-    let sampleCard = cards[index];
+
+    //On button click we get next card in deck and default state to show question
+    const knownClick = (e : React.MouseEvent<HTMLButtonElement>) => {
+        updateIndex(loopIndex);
+        updateDirection(true);
+        
+    }
+
+    const unknownClick = (e : React.MouseEvent<HTMLButtonElement>) => {
+        updateIndex(loopIndex);
+        updateDirection(true);
+
+    }
 
     
 
+    let currentCard = activeCards[index];
+    
     return (
         <div className='bg-gray-900 h-screen flex flex-col space-y-3 items-center justify-center select-none'>
-            <Card question={sampleCard.question} answer={sampleCard.answer} isKnown={sampleCard.isKnown} />
+            <CardRender displayText={frontFacing ? currentCard.question : currentCard.answer} func={flipCard}/>
 
             <div className='space-x-3'>
-                <Button clickHandler={handleClick} displayTag="I don't know it"></Button>
-                <Button clickHandler={handleClick} displayTag='I know it'></Button>
+                <Button clickHandler={unknownClick} displayTag="I Don't Know This One"></Button>
+                <Button clickHandler={knownClick} displayTag='I Know This One'></Button>
             </div>
         </div>
     );
