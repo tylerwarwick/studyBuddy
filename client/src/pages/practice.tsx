d21b6655c3531b38e42d77ae8578dbc6195e1531
tylerwarwick <tylerwarwickwork@gmail.com>
2023-07-27 23:46:31 -0400

    Some progress with practice page

--- client/src/pages/practice.tsx
+++ client/src/pages/practice.tsx
@@ -4,6 +4,7 @@ import {useState, useEffect} from 'react';
 import CardRender from '../components/cardRender';
 import Button from '../components/button';
 import { useParams } from 'react-router-dom';
+import { createTypeReferenceDirectiveResolutionCache } from 'typescript';
 
 interface Card {
     id : number;
@@ -17,62 +18,94 @@ type deckID = {a : string}
 
 const Practice = () => {
     const [cards, setCards] = useState<Card[]>([]);
+    const [uC, setUc] = useState<Card[]>([])
+    
     const params = useParams<deckID>() as string;
 
     //Fetch data from server on refresh
     useEffect(() => {
-        axios.get("http://localhost:3001/classes")
-        .then(Response => {
-            setCards(cards.concat(Response.data[Object.values(params)[0]] as Card[]))
-            console.log(Object.values(params)[0])
-            });
-
+        const getData = async () => {
+            const data = (await axios.get("http://localhost:3001/classes")).data;
+            const arr = data[Object.values(params)[0]] as Card[]
+            setCards(arr)
+            setUc(arr.filter((c) => {return c.isKnown == false}))
+        }
+        
+        getData();
         
     }, [])
     
    
-    //Get cards that aren't memorized yet
-    const checkIsKnown = (card : Card) => {if (!card.isKnown) return card};    
-    let activeCards = cards.filter(checkIsKnown);
-     
     //Store length of question deck to infinitely cycle through deck until empty
     const [index, updateIndex] = useState(0);
     
-     
+    
+    
     //Define state for which way card is facing
     //Pass function to update state to child card component
     const [frontFacing, setFrontFacing] = useState(true);
     const flipCard = () => setFrontFacing(frontFacing => !frontFacing);
      
     //Loop through deck forever
-    const loopIndex = () => {return index == activeCards.length - 1 ? 0 : index + 1};
+    const loopIndex = () => {
+        return index == uC.length - 1 ? updateIndex(() => 0) : updateIndex(() => index+1)
+    
+    }
+
+    const updateKnown = uC.map((c) => {
+        if (c.id === uC[index].id) return {...c, isKnown : true}
+
+        return c;
+    })
      
     //On button click we get next card in deck and default state to show question
     const knownClick = () => {
-        updateIndex(loopIndex);
+        setUc(updateKnown)
+        setUc((uC)=>uC.filter((c)=> { return c.isKnown == false } ))
         setFrontFacing(true); 
+       
+        console.log(uC)
     }
      
     const unknownClick = () => {
-        updateIndex(loopIndex);
+        loopIndex()
         setFrontFacing(true);
      
     }
      
-         
-     
-    let currentCard = activeCards[index];
-         
-    return (
+    const resetDeck = () => {
+        setCards((cards) => cards.map((c) => {
+            return {...c, isKnown : false}
+        }))
+
+        const copy = cards
+        setUc(copy)
+
+        console.log(cards);
+        console.log(uC);
+    }
+
+    if (uC.length == 0) {
+        return (
+            <div className='bg-gray-900 h-screen flex flex-col space-y-3 items-center justify-center select-none'>
+                <CardRender displayText={"You've completed all of the questions"} func={flipCard}/>
+                <button onClick={resetDeck} className='text-white focus:ring-4 focus:outline-none font-bold rounded-lg text-lg w-96 h-16 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800'>Reset Deck</button>
+            </div>
+            );
+    }
+
+    else {
+        return (
         <div className='bg-gray-900 h-screen flex flex-col space-y-3 items-center justify-center select-none'>
-            <CardRender displayText={frontFacing ? currentCard?.question : currentCard?.answer} func={flipCard}/>
-     
+            <CardRender displayText={frontFacing ? uC[index]?.question : uC[index]?.answer} func={flipCard}/>
+    
             <div className='space-x-3'>
                 <Button clickHandler={unknownClick} displayTag="I Don't Know This One"></Button>
                 <Button clickHandler={knownClick} displayTag='I Know This One'></Button>
             </div>
         </div>
-    );
+        );
+    }
 }
 
 export default Practice;
