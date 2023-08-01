0791155c9b7e7cdbbdbb7b3e043355ab35c11c13
tylerwarwick <tylerwarwickwork@gmail.com>
2023-08-01 00:35:08 -0400

    Decklobby is responsive now :)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ import EditDeck from './pages/editDeck';
 function App() {
   
   return (
-    <div className='bg-gray-900 h-screen'>
+    <div className='bg-gray-900 min-h-screen min-w-screen'>
       <BrowserRouter>
         <div className='z-50 sticky top-0'>
           <Navbar />
--- client/src/pages/deckLobby.tsx
+++ client/src/pages/deckLobby.tsx
@@ -19,17 +19,20 @@ export default function DeckLobby() {
             });
     }, [])
 
+    //grid [@media(min-width:1290px)]:grid-cols-3 gap-10 [@media(max-width:1289px)]:grid-cols-2 [@media(max-width:880px)]:grid-cols-1
 
     return (
-        <div className="min-h-screen h-fit bg-gray-900 min-w-screen p-8">
-            <div className="flex justify-center m-6">
-                <div className="grid lg:grid-cols-3 gap-12 md:grid-cols-2">
-                {decks.map((deckName, index) => (
+        
+        <div className="min-h-screen h-fit bg-gray-900 min-w-full w-fit p-8">
+            <div className="flex justify-center items-center m-6">
+                <div className="grid [@media(min-width:1290px)]:grid-cols-3 gap-10 [@media(max-width:1289px)]:grid-cols-2 [@media(max-width:880px)]:grid-cols-1">
+                    {decks.map((deckName, index) => (
                         <DeckCard decks={decks} deckIndex={index} setDecks={setDecks}/>             
                         ))}
                     <NewDeck decks={decks} setDecks={setDecks}/>
                 </div>
             </div>
         </div>
+       
     );
 }
