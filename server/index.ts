53f41f55c057d96aa836314a782c73deee572479
tylerwarwick <tylerwarwickwork@gmail.com>
2023-08-13 15:55:14 -0400

    Housekeeping

--- server/index.ts
+++ server/index.ts
@@ -25,7 +25,7 @@ app.use(verifyToken);
 app.use('/deck', deckRouter);
 app.use('/card', cardRouter);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
