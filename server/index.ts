1cbae40e89ea0ae63d84b7c6615976230e821171
tylerwarwick <tylerwarwickwork@gmail.com>
2023-08-13 20:03:12 -0400

    Rate limiting is provided by render so it's not necessary

--- server/index.ts
+++ server/index.ts
@@ -5,25 +5,15 @@ import deckRouter from './controllers/decks';
 import { verifyToken } from './controllers/auth';
 import cardRouter from './controllers/cards';
 import registerRouter from './controllers/register';
-import RateLimit from 'express-rate-limit';
 
 
-const app = express()
+
+const app = express();
+
 app.use(cors({
   origin: 'https://studybuddy-q2tg.onrender.com/'
 })); 
 app.use(express.json())
-
-
-// Set up rate limiter: maximum of twenty requests per minute
-const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 20,
-});
-// Apply rate limiter to all requests
-app.use(limiter);
-
-
 
 //Login and registration is allowed without token (obviously)
 app.use('/register', registerRouter);
