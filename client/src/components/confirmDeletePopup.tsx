7d48533db504947278fadbf569ed9764c55be8df
tylerwarwick <tylerwarwickwork@gmail.com>
2023-08-14 22:16:26 -0400

    Fixed last two light mode colors

--- client/src/components/confirmDeletePopup.tsx
+++ client/src/components/confirmDeletePopup.tsx
@@ -35,7 +35,7 @@ export default function ConfirmDeletePopup({setConfirmHidden: setConfirmHidden, 
     return(
         <div id="deleteModal" tabIndex={-1} aria-hidden="true" className="overflow-y-auto overflow-x-hidden fixed flex justify-center items-center z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
         <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-            <div className="relative p-4 text-center rounded-lg shadow bg-gray-800 sm:p-5 border-2">
+            <div className="relative p-4 text-center rounded-lg shadow bg-gray-800 sm:p-5 border-2 border-gray-600">
                 <button onClick={() => setConfirmHidden(true)} type="button" className="text-gray-400 absolute top-2.5 right-2.5 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-600 hover:text-white" data-modal-toggle="deleteModal">
                     <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     <span className="sr-only">Close modal</span>
--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -18,7 +18,7 @@ const NavLinkStyled = ({displayTag: displayTag, location: location} : NavLinkPro
     <li>
         <NavLink to={location} 
             className={({ isActive, isPending }) =>
-            isPending ? "pending" : (isActive ? 'bg-blue-500 md:bg-transparent md:text-blue-500 ' : '') + 'block py-2 pl-3 pr-4 rounded md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent'}> 
+            isPending ? "pending" : (isActive ? 'bg-blue-500 md:bg-transparent md:text-blue-500 ' : '') + 'block py-2 pl-3 pr-4 rounded md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent md:bg-transparent'}> 
                     {displayTag} 
         </NavLink>
     </li>
@@ -41,14 +41,14 @@ export default function Navbar() {
         <nav className="bg-gray-900 border-gray-600 border-b">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href={'/'} className="flex items-center">
-                <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">StudyBuddy</span>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap text-white hover:text-blue-500">StudyBuddy</span>
             </a>
             <button onClick={handleClick} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
             </button>
             <div className={(hiding ? "hidden " : "") + "w-full md:block md:w-auto"} id="navbar-default" >
-                <ul id='navbar' className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white bg-gray-800 md:bg-gray-900 border-gray-700">
+                <ul id='navbar' className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent bg-gray-800 md:bg-gray-900 border-gray-700">
                     {(user === null) ? (<>
                     <NavLinkStyled displayTag='Login' location='/login'/>
                     <NavLinkStyled displayTag='Register' location='/register'/>
@@ -56,7 +56,7 @@ export default function Navbar() {
                         <NavLinkStyled displayTag='Decks' location='/decks'/>
                         <NavLink to={'/login'} onClick={()=> {LoginService.logout(); setUser(null)}}
                             className={({ isActive, isPending }) =>
-                            isPending ? "pending" : (isActive ? 'bg-blue-500 md:bg-transparent md:text-blue-500 ' : '') + 'block py-2 pl-3 pr-4 rounded md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent'}> 
+                            isPending ? "pending" : (isActive ? 'bg-blue-500 md:bg-transparent md:text-blue-500 ' : '') + 'block py-2 pl-3 pr-4 rounded md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent md:bg-transparent'}> 
                                     Logout
                         </NavLink>
                     </>)}
